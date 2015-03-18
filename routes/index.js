54d12b2c152804f1ac4967eb3584ddf321a73a76
pixding <djqq1987@gmail.com>
2015-03-18 23:45:55 +0800

    #

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
-//var noteMod = require('../models');
+
 /*
  * GET home page.
  */
-
+var noteMod = require('../models');
+var dateFormat = require('dateformat');
 exports.index = function(req, res){
-    var proxy_ip=req.headers['x-real-ip'] || req.headers['x-forwarded-for']
-    if(proxy_ip) {
-        console.log("proxy:" + proxy_ip);
-    }
-    if(req.ip) {
-        console.log(req.ip);
-    }
-    //console.log("ua:"+req.headers['user-agent']+".........ip:"+req._remoteAddress+"\r\n");
+    var ip=req.headers['x-real-ip'] || req.headers['x-forwarded-for']||req.ip;
+    var createDate = dateFormat(new Date(), "yyyy-mm-dd");
+    var ua = req.headers["user-agent"];
+    var referer = req.headers["referer"];
+
     res.json({"a":"1"});
 
-
 };
+
+
